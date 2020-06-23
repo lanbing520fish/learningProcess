1e2a6d8e18e698afb2fae2787a2fd0d3fcff6a85
heyue <289383680@qq.com>
2020-06-23 16:09:14 +0800

    bugfix

--- src/view/Sass.vue
+++ src/view/Sass.vue
@@ -7,6 +7,7 @@
       <code>npm install --save-dev sass-loader   //sass-loader依赖于node-sass
       npm install --save-dev node-sass</code>
     </pre>
+    <br />
     <h4>2、配置: 在build文件夹下的webpack.base.conf.js的rules里面添加配置</h4>
     <pre>
       <code>// 如果你是.sass文件要设置
@@ -15,10 +16,17 @@
         loaders: ['style', 'css', 'sass']
       }</code>
     </pre>
+    <br />
     <h4>3、在APP.vue中修改style标签</h4>
     <pre>
       <code>{{scss}}</code>
     </pre>
+    <br />
+    <h4>4、问题：Module build failed: TypeError: this.getResolve is not a function at Object.loader</h4>
+    <p>
+      解决：
+      <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;这是因为sass-loader版本太高了，去package.json文件里面的 "sass-loader"的版本更换成7.3.1，然后重新npm install就可以了
+    </p>
   </div>
 </template>
 
